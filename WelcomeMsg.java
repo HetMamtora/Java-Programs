6de4f0c21bc9792d0035d88e7b9f79fe1e1867ff
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2024-01-30 16:54:54 +0530

    updated

---
+++ WelcomeMsg.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+class WelcomeMsg
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+
+		String name;
+		System.out.println("May I know your name ?");
+		name=s.nextLine();
+		System.out.println("Welcome Mr/Mrs." +name);
+	}
+}
