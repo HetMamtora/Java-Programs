06c29c3512a7279f0ed095844e3d777bc34b5d64
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-05 15:20:16 +0530

    updated

--- DataSizeRange.java
+++ DataSizeRange.java
@@ -5,6 +5,12 @@ class DataSizeRange
 {
 	public static void main(String args[])
 	{
-		int x;
+		System.out.println("Int Min: " +Integer.MIN_VALUE);
+		System.out.println("Int Max: " +Integer.MAX_VALUE);
+		System.out.println("Int Bytes: " +Integer.BYTES);
+		
+		System.out.println("Byte Min: " +Byte.MIN_VALUE);
+		System.out.println("Byte Max: " +Byte.MAX_VALUE);
+		System.out.println("Byte Bytes: " +Byte.BYTES);
 	}
 }
