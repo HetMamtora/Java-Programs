cf00d429a37f90adc1e42998ede94131dd6c35b8
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-05 15:58:45 +0530

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -5,7 +5,17 @@ class Variables
 {
 	public static void main(String args[])
 	{
-		byte b=128;
+		byte b=127;
+		short s=300;
+		int i=100;
+
+		float f=44.4f;
+		char c='A';
+		
 		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(c);
 	}
 }
