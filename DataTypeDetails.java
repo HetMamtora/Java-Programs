a00bb2d64421f003f7c80d9a485c78dbc0efea7d
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-06 20:05:27 +0530

    Update DataTypeDetails.java

--- DataTypeDetails.java
+++ DataTypeDetails.java
@@ -1,3 +1,14 @@
 import java.lang.*;
 import javax.swing.*;
 import java.awt.*;
+
+
+class DataTypeDetails
+{
+	public static void main(String args[])
+	{
+		//Binary form of 5		
+		int x=5;
+		
+	}
+}
