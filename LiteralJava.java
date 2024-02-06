b58d463ec63f6fc618858c28827090a00c188418
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-06 19:38:56 +0530

    updated

---
+++ LiteralJava.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+
+class LiteralJava
+{
+	public static void main(String args[])
+	{
+		//INTEGER LITERAL
+		byte b1=10;
+		byte b2=0b1010;
+		byte b3=012;
+		byte b4=0XA;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+
+		//LONG LITERAL
+		long l=999999999999L;
+		System.out.println(l);
+	}
+}
