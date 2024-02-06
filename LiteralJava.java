7bb22acb2750589534514601a9758abaac628de9
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-06 19:43:50 +0530

    Update LiteralJava.java

--- LiteralJava.java
+++ LiteralJava.java
@@ -22,7 +22,13 @@ class LiteralJava
 		//FLOAT & DOUBLE
 		float f=12.56f;
 		double d=12.56d;
+
+		long l1=999_999_999;
+		float f1=123_456.56f;
+
 		System.out.println(f);
 		System.out.println(d);
+		System.out.println(l1);
+		System.out.println(f1);
 	}
 }
