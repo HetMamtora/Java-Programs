d0eef27a10a025e6228ef751a380d629de9d5785
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-08 20:04:40 +0530

    Create QuadraticEq.java

---
+++ QuadraticEq.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class QuadraticEq
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+
+		int a,b,c;
+		double r1,r2;
+
+		System.out.println("Enter value for A, B & C:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+	}
+}
