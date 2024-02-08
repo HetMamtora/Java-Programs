73ea7c53921b400e7a673298887a7909df947506
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-08 22:16:09 +0530

    updated

---
+++ IncrementDecrement/src/incrementdecrement/IncrementDecrement.java
@@ -0,0 +1,14 @@
+package incrementdecrement;
+
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+        
+        int x=5;
+        x++;
+        System.out.println(x);
+        ++x;
+        System.out.println(x);
+    }
+    
+}
