49a50217959912c6399788dcf3960dc5cebaec04
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-08 16:53:41 +0530

    Update AreaOfTriangle.java

--- AreaOfTriangle.java
+++ AreaOfTriangle.java
@@ -5,8 +5,11 @@ class AreaOfTriangle
 {
 	public static void main(String args[])
 	{
-		float base,height,area;
 		Scanner sc=new Scanner(System.in);
+	
+		//TYPE-1
+
+		float base,height,area;
 
 		System.out.println("Enter Base: ");
 		base=sc.nextFloat();
@@ -14,8 +17,22 @@ class AreaOfTriangle
 		height=sc.nextFloat();
 
 		area=base*height/2;
-		System.out.println("Area of Triangle: "+area);
+		System.out.println("Area of Triangle1: "+area);
+		
+		//TYPE-2
+		int a,b,c;
+		float s;
+		double area1;
+		
+		System.out.println("Enter 3 sides of a Triangle: ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
 
+		s=(a+b+c)/2f;
+		area1=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+		System.out.println("Area of Triangle2:");
+		
 	}
 
 }
