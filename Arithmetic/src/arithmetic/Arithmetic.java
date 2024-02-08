bd5fcf323f55a4e637a13ca85b2d81eea2fffe9e
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-08 15:51:53 +0530

    updated

--- Arithmetic/src/arithmetic/Arithmetic.java
+++ Arithmetic/src/arithmetic/Arithmetic.java
@@ -4,12 +4,20 @@ public class Arithmetic
 {
     public static void main(String[] args)
     {
+        //INTEGER DATA TYPE
         int a=14, b=5;
         int c=a/b;
         int r=a%b;
+        //type cast
+        float c1=(float)a/b;
+        float r1=(float)a%b;
         
         System.out.println("C: "+c);
         System.out.println("R: "+r);
+        System.out.println("C1: "+c1);
+        System.out.println("R1: "+r1);
+        
+        //FLOAT DATA TYPE
     }
     
 }
