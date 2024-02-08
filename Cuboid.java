3624ca760efe975fd7777119a6b2cfded10a04d4
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-08 21:59:19 +0530

    Update Cuboid.java

--- Cuboid.java
+++ Cuboid.java
@@ -5,6 +5,19 @@ class Cuboid
 {
 	public static void main(String args[])
 	{
-	
+		Scanner sc=new Scanner(System.in);
+
+		int length,breadth,height;
+		int area, volume;
+
+		System.out.println("Enter value for Length, Breadth & Height:");
+		length=sc.nextInt();
+		breadth=sc.nextInt();
+		height=sc.nextInt();
+
+		area=(length*height)+(length*breadth)+(breadth*height);
+		System.out.println("Area of Cuboid: " +area);
+
+		volume=length*breadth*height;
 	}
 }
