fc283cb6f47a7f5959fcef368d96cc3f5330ac6b
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-08 22:19:34 +0530

    Update IncrementDecrement.java

--- IncrementDecrement/src/incrementdecrement/IncrementDecrement.java
+++ IncrementDecrement/src/incrementdecrement/IncrementDecrement.java
@@ -9,6 +9,18 @@ public class IncrementDecrement {
         System.out.println(x);
         ++x;
         System.out.println(x);
+        
+        int x1=5,y1;
+        y1=x1++;
+        System.out.println(x1+" "+y1);
+        y1=++x1;
+        System.out.println(x1+" "+y1);
+        
+        int x2=5,y2=4,z2;
+        z2=2*x2++ + 3 * ++y2;
+        System.out.println(z2);
+        
+        
     }
     
 }
