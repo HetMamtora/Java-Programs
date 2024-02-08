957369f29e141a98cfd2f4be5e77ff596decd778
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-08 15:48:31 +0530

    updated

---
+++ Arithmetic/src/arithmetic/Arithmetic.java
@@ -0,0 +1,15 @@
+package arithmetic;
+
+public class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        int a=14, b=5;
+        int c=a/b;
+        int r=a%b;
+        
+        System.out.println("C: "+c);
+        System.out.println("R: "+r);
+    }
+    
+}
