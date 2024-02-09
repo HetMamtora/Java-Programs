860a80c534c8ab7822ecbb076d7c7a32726c69fd
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-09 16:27:46 +0530

    updated

--- Bitwise.java
+++ Bitwise.java
@@ -5,11 +5,22 @@ class Bitwise
 	public static void main(String args[])
 	{
 		//SWAPING
-		int a=10,b=15;
-		a=a^b;
-		b=a^b;
-		a=a^b;
-		System.out.println(a);
-		System.out.println(b);
+		int r1=10,r2=15;
+		r1=r1^r2;
+		r2=r1^r2;
+		r1=r1^r2;
+		System.out.println(r1);
+		System.out.println(r2);
+
+
+		//SWAPING-2
+		byte a=9,b=12;
+		byte c;
+
+		c=(byte)(a<<4);
+		c=(byte)(c|b);
+
+		System.out.println((c&0b11110000)>>4);
+		System.out.println((c&0b00001111));
 	}
 }
