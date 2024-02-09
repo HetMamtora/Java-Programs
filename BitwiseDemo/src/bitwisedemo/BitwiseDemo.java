95b5e875e8645c63d2f4171568d05134c220cf22
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-09 12:52:38 +0530

    updated

---
+++ BitwiseDemo/src/bitwisedemo/BitwiseDemo.java
@@ -0,0 +1,12 @@
+package bitwisedemo;
+
+public class BitwiseDemo {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=10, y=6, z;
+        z=x&y;
+        System.out.println(z);
+    }
+    
+}
