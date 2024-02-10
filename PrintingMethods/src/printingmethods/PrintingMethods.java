b37b0596551651a50e83c9f93269571f1cf118b8
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-10 19:14:27 +0530

    updated

--- PrintingMethods/src/printingmethods/PrintingMethods.java
+++ PrintingMethods/src/printingmethods/PrintingMethods.java
@@ -6,10 +6,15 @@ public class PrintingMethods {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int x=10;
+        int a=10;
         float b=12.55f;
         char c='A';
         String str="Hello";
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(str);
     }
     
 }
