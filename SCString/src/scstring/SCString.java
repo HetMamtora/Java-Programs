d28ffaa587b3ecb036fa0bce3c10f4bc35ec29e9
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-11 20:57:16 +0530

    updated

---
+++ SCString/src/scstring/SCString.java
@@ -0,0 +1,22 @@
+package scstring;
+
+public class SCString {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String str="abcd@gmail.com";
+        
+        int i=str.indexOf("@");
+        String uname=str.substring(0,i);
+        String domain=str.substring(i+1, str.length());
+        
+        System.out.println("USERNAME: "+uname);
+        System.out.println("DOMAIN: "+domain);
+        
+        int j=domain.indexOf(".");
+        String name=domain.substring(0,j);
+        System.out.println(name.equals("gmail"));
+    }
+    
+}
