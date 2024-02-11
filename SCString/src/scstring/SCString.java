d9554dbeef9c42cf590ff77a9b564ddfe3ce63ed
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-11 21:12:44 +0530

    Update SCString.java

--- SCString/src/scstring/SCString.java
+++ SCString/src/scstring/SCString.java
@@ -5,9 +5,11 @@ public class SCString {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //FIND IF THE EMAIL ID IS ON GMAIL OR NOT
-        
+        //1-FIND IF THE EMAIL ID IS ON GMAIL OR NOT
+        System.out.println("1-FIND IF THE EMAIL ID IS ON GMAIL OR NOT");
         String str="abcd@gmail.com";
+        
+        System.out.println(str);
         
         int i=str.indexOf("@");
         String uname=str.substring(0,i);
@@ -19,6 +21,20 @@ public class SCString {
         int j=domain.indexOf(".");
         String name=domain.substring(0,j);
         System.out.println(name.equals("gmail"));
+        
+        
+        //2-FIND IF NUMBER IS BINARY OR NOT
+        System.out.println("\n2-FIND IF NUMBER IS BINARY OR NOT");
+        int b=1001110;
+        String str1=String.valueOf(b);
+        System.out.println(b);
+        System.out.println(str1.matches("[01]+"));
+        
+        //3-FIND IF NUMBER IS HEXA-DECIMAL OR NOT
+        System.out.println("\n3-FIND IF NUMBER IS HEXA-DECIMAL OR NOT");
+        String str2="234AB";
+        System.out.println(str2);
+        System.out.println(str2.matches("[0-9A-F]+"));
     }
     
 }
