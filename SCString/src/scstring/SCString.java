72d0a91d618e842dde7c3c59e7a138068bfdd0a9
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-11 20:58:06 +0530

    Update SCString.java

--- SCString/src/scstring/SCString.java
+++ SCString/src/scstring/SCString.java
@@ -5,6 +5,8 @@ public class SCString {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        //FIND IF THE EMAIL ID IS ON GMAIL OR NOT
+        
         String str="abcd@gmail.com";
         
         int i=str.indexOf("@");
