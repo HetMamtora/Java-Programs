d317814227d19d5d95c6cb487c66fe3f055530fc
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-12 20:59:54 +0530

    updated

---
+++ TypeofWebsite/src/typeofwebsite/TypeofWebsite.java
@@ -0,0 +1,27 @@
+package typeofwebsite;
+import java.util.*;
+
+public class TypeofWebsite {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner sc=new Scanner(System.in);
+        
+        String url;
+        System.out.println("Enter the URL: ");
+        url=sc.nextLine();
+        
+        String protocol=url.substring(0,url.indexOf(":"));
+        
+        if(protocol.equals("http"))
+        {
+            System.out.println("HyperText Transfer Protocol");
+        }
+        else if(protocol.equals("ftp"))
+        {
+            System.out.println("File Transfer Protocol");
+        }
+    }
+    
+}
