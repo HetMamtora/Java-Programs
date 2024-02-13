c134664c5d0bc091f1d26eaeec93eb680d498b0a
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-13 12:52:23 +0530

    updated

---
+++ SwitchCase/src/switchcase/SwitchCase.java
@@ -0,0 +1,22 @@
+package switchcase;
+
+public class SwitchCase {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int n=2;
+        
+        switch(n)
+        {
+            case 1: System.out.println("One");
+                    break;
+            case 2: System.out.println("Two");
+                    break;
+            case 3: System.out.println("Three");
+                    break;
+            default: System.out.println("Invalid");
+        }
+    }
+    
+}
