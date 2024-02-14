fec85f37397a4df670f8c644ca107470e003caeb
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-14 22:33:05 +0530

    updated

---
+++ DisplayNumberinWords/src/displaynumberinwords/DisplayNumberinWords.java
@@ -0,0 +1,17 @@
+package displaynumberinwords;
+import java.util.*;
+
+public class DisplayNumberinWords {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        
+        int n;
+        System.out.println("Enter Number: ");
+        n=sc.nextInt();
+        
+        
+    }
+    
+}
