444b357dba58fcd5865ed40c3151c5d82c583b22
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-14 15:16:07 +0530

    updated

---
+++ ForLoopPrac/src/forloopprac/ForLoopPrac.java
@@ -0,0 +1,19 @@
+package forloopprac;
+
+public class ForLoopPrac {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int i=0; i<=10; i++)
+        {
+            System.out.println(i);
+        }
+        
+        for(int j=10; j>=0; j--)
+        {
+            System.out.println(j);
+        }
+    }
+    
+}
