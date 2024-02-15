46a6366a964c44e6007622eb2859f12356e89320
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-15 16:29:51 +0530

    updated

---
+++ Pattern1/src/pattern1/Pattern1.java
@@ -0,0 +1,43 @@
+package pattern1;
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("Pattern-1: ");
+        int i,j;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=5;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+        
+        System.out.println("\nPattern-2: ");
+        int i1,j1;
+        for(i1=1;i1<=5;i1++)
+        {
+            for(j1=1;j1<=5;j1++)
+            {
+                System.out.print(i1+" ");
+            }
+            System.out.println("");
+        }
+        
+        System.out.println("\nPattern-3: ");
+        int i2,j2;
+        for(i2=1;i2<=5;i2++)
+        {
+            for(j2=1;j2<=5;j2++)
+            {
+                System.out.print(i2+j2+" ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
