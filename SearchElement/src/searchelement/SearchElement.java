f36fe34be9f86604397ea76316791ebc007d1edf
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-16 16:10:02 +0530

    updated

--- SearchElement/src/searchelement/SearchElement.java
+++ SearchElement/src/searchelement/SearchElement.java
@@ -1,20 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package searchelement;
+import java.util.*;
 
-/**
- *
- * @author hbmam
- */
 public class SearchElement {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        
+        int key,i;
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        
+        System.out.println("Enter element to Search:");
+        key=sc.nextInt();
+        
+        for(i=0; i<=A.length; i++)
+        {
+            if(A[i]==key)
+            {
+                System.out.println("Element found at: "+i);
+                System.exit(0);
+            }
+        }
+        System.out.println("Not Found");
     }
-    
 }
