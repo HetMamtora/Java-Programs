4dea4552f727389cdf8a5d31201f01574ce6ce9e
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-17 17:19:53 +0530

    updated

---
+++ MethodsPractice1/src/methodspractice1/MethodsPractice1.java
@@ -0,0 +1,32 @@
+package methodspractice1;
+
+public class MethodsPractice1
+{
+    static void change(int A[], int index, int value)
+    {
+        A[index]=value;
+    }
+    
+    static void change2(int x, int value)
+    {
+        x=value;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int A[]={2,4,6,8,10};
+        
+        change(A,2,20);
+        
+        for(int x:A)
+        {
+            System.out.println(x);
+        }
+        
+        int x=10;
+        change2(x,20);
+        System.out.println("Value of Primitive Datatype: "+x);
+    }
+    
+}
