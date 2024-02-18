a9b8bf4e4f9b9d0a66dcfafa78bf1ec13811d4e9
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-18 20:55:56 +0530

    updated

---
+++ VarargsMaxNo/src/varargsmaxno/VarargsMaxNo.java
@@ -0,0 +1,29 @@
+package varargsmaxno;
+
+public class VarargsMaxNo
+{
+    static int max(int ...A)
+    {
+        if(A.length==0)
+            return Integer.MIN_VALUE;
+        
+        int max=A[0];
+        for(int i=1;i<A.length;i++)
+            if(A[i]>max)
+                max=A[i];
+        
+        return max;
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println(max());
+        System.out.println(max(10));
+        System.out.println(max(10,20));
+        System.out.println(max(10,20,30));
+
+    }
+    
+}
