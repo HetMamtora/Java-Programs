02632bc0f63a6a63bed981c4f5a7cc6415c79d86
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-23 15:06:48 +0530

    updated

---
+++ AbstractClassDoDont/src/abstractclassdodont/AbstractClassDoDont.java
@@ -0,0 +1,27 @@
+//MAKE CHANGES AND UNDERSTAND ACCORDING TO ABSTRACT CLASS RULES
+
+package abstractclassdodont;
+
+abstract class Super
+{
+    abstract public void meth1();
+}
+
+class Sub extends Super
+{
+    @Override
+    public void meth1()
+    {
+        
+    }
+}
+
+public class AbstractClassDoDont {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         Super s;
+    }
+    
+}
