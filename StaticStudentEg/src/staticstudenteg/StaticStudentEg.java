48c87940671874702715178907f17e12c5130ae1
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-24 16:36:38 +0530

    updated

---
+++ StaticStudentEg/src/staticstudenteg/StaticStudentEg.java
@@ -0,0 +1,41 @@
+package staticstudenteg;
+import java.util.Date;
+
+class Student
+{
+    private String rollNo;
+    private static int count=1;
+    
+    private String assignRollNo()
+    {
+        Date d=new Date();
+        
+        String rno="Uni-"+(d.getYear()+1900)+"-"+count;
+        count++;
+        return rno;
+    }
+    Student()
+    {
+        rollNo=assignRollNo();
+    }
+    public String getRollNo()
+    {
+        return rollNo;
+    }
+}
+
+public class StaticStudentEg {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Student s1=new Student();
+        Student s2=new Student();
+        Student s3=new Student();
+        
+        System.out.println(s1.getRollNo());
+        System.out.println(s2.getRollNo());
+        System.out.println(s3.getRollNo());
+    }
+    
+}
