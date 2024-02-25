472e41f63b17aecc9072276908de62e08008e3da
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-25 13:21:08 +0530

    updated

---
+++ AccessModifierDemo/src/accessmodifierdemo/AccessModifierDemo.java
@@ -0,0 +1,52 @@
+package accessmodifierdemo;
+
+class Demo1
+{
+    int a=10;
+    //private.
+    public int b=20;
+    protected int c=30;
+    public int d=40;
+    
+    public void display()
+    {
+        System.out.println(a+b+c+d);
+    }
+}
+/*Public class Demo2()
+{
+    Demo1 d=new Demo1();
+    public void show()
+    {
+        System.out.println(d.a+d.b+d.c+d.d);
+    }
+    
+}*/
+class Demo3 extends Demo1
+{
+    Demo1 d=new Demo1();
+    public void show()
+    {
+        System.out.println(d.a+d.b+d.c+d.d);
+    }
+}
+
+class Demo4 extends Demo1
+{
+    public void show()
+    {
+        System.out.println(a+b+c+d);
+    }
+}
+
+public class AccessModifierDemo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Demo1 d1=new Demo1();
+        d1.display();
+        System.out.println(d1.a+d1.b+d1.c+d1.d);
+    }
+    
+}
