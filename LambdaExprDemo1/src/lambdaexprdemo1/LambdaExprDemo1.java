7beed73d76e732d86ea1a460f5259fbf8c3e1112
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-02-29 15:11:45 +0530

    Update LambdaExprDemo1.java

--- LambdaExprDemo1/src/lambdaexprdemo1/LambdaExprDemo1.java
+++ LambdaExprDemo1/src/lambdaexprdemo1/LambdaExprDemo1.java
@@ -1,9 +1,18 @@
 package lambdaexprdemo1;
 
-@FunctionalInterface
+//DEMO-1
+/*@FunctionalInterface
 interface MyLambda
 {
     public void display();
+}  DEMO-1 */
+
+//DEMO-2 [PARAMETERS IN LAMBDA]
+@FunctionalInterface
+interface MyLambda
+{
+    //public void display(String str);
+    public int add(int x,int y);
 }
 
 /*class My implements MyLambda
@@ -19,10 +28,18 @@ public class LambdaExprDemo1 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        
+        /* DEMO - 1
         //MyLambda m=new My();
         //THIS IS ANONYMOUS METHOD WHICH IS LAMBDA EXPRESSION
         MyLambda m=()->{ System.out.println("Hello World"); };
-        m.display();
+        m.display(); */  //DEMO-1
+        
+        //DEMO-2
+        /*MyLambda m=(s)->{ System.out.println(s); };
+        m.display("Hello World");*/
+        MyLambda m=(a,b)->{ return a+b; };
+        System.out.println(m.add(20,30));
         
     }
     
