0ed537257fa98a9ff24f1d69314e42f07cbd137e
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-01 17:04:23 +0530

    updated

---
+++ PipedStreamsEg/src/pipedstreamseg/PipedStreamsEg.java
@@ -0,0 +1,82 @@
+package pipedstreamseg;
+import java.io.*;
+
+class Producer extends Thread
+{
+    OutputStream os;
+    
+    public Producer(OutputStream o)
+    {
+        os=o;
+    }
+    
+    public void run()
+    {
+        int count=1;
+        
+        while(true)
+        {
+            try{
+            os.write(count);
+            os.flush();
+            
+            System.out.println("Producer: "+count);
+            System.out.flush();
+
+            Thread.sleep(10);
+            count++;
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+    
+}
+
+class Consumer extends Thread
+{
+    InputStream is;
+    
+    public Consumer(InputStream s)
+    {
+        is=s;
+    }
+    
+    public void run()
+    {
+        int x;
+        
+        while(true)
+        {
+            try{
+            
+                x=is.read();
+            
+            System.out.println("Consumer: "+x);
+            System.out.flush();
+            Thread.sleep(10);
+
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+    
+}
+public class PipedStreamsEg {
+
+    public static void main(String[] args) throws Exception{
+        // TODO code application logic here
+        
+       PipedInputStream pis=new PipedInputStream();
+       PipedOutputStream pos=new PipedOutputStream();
+       
+       pos.connect(pis);
+       
+       Producer p=new Producer(pos);
+       Consumer c=new Consumer(pis);
+       
+       p.start();
+       c.start();
+    }
+}
