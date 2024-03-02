fd272d37c30d6c3d8da5db18369d38e6d5ba0a3a
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-02 16:40:18 +0530

    updated

--- SerializeCustomer/src/serializecustomer/SerializeCustomer.java
+++ SerializeCustomer/src/serializecustomer/SerializeCustomer.java
@@ -1,5 +1,6 @@
 package serializecustomer;
 import java.io.*;
+import java.util.*;
 
 class Customer implements Serializable
 {
@@ -32,7 +33,8 @@ public class SerializeCustomer {
     public static void main(String[] args) throws Exception{
         // TODO code application logic here
         
-        Customer list[]={new Customer("ABC","1234567890"),new Customer("BCD","2345678901"),new Customer("CDE","3456789012")};
+        //WRITE INTO FILE
+        /*Customer list[]={new Customer("ABC","1234567890"),new Customer("BCD","2345678901"),new Customer("CDE","3456789012")};
         
         FileOutputStream fos=new FileOutputStream("Customer.txt");
         ObjectOutputStream oos=new ObjectOutputStream(fos);
@@ -43,7 +45,34 @@ public class SerializeCustomer {
             oos.writeObject(c);
         
         oos.close();
-        fos.close();
+        fos.close();*/
+        
+        
+        //READ FROM FILE 
+        Scanner sc=new Scanner(System.in);
+        
+        FileInputStream fis=new FileInputStream("Customer.txt");
+        ObjectInputStream ois=new ObjectInputStream(fis);
+        
+        int length=ois.readInt();
+        
+        Customer list[]=new Customer[length];
+        
+        for(int i=0; i<length; i++)
+        {
+            list[i]=(Customer)ois.readObject();
+        }
+        
+        System.out.print("Enter name of Customer: ");
+        String name=sc.nextLine();
+        
+        for(int i=0; i<length; i++)
+        {
+            if(name.equalsIgnoreCase(list[i].name))
+                System.out.println(list[i]);
+        }
+        ois.close();
+        fis.close();
     }
     
 }
