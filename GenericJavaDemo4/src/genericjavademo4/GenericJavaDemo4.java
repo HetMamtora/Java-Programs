eecb34bcc006c4920ffdfb9ec542b2f9e35368e1
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-03 16:55:10 +0530

    Update GenericJavaDemo4.java

--- GenericJavaDemo4/src/genericjavademo4/GenericJavaDemo4.java
+++ GenericJavaDemo4/src/genericjavademo4/GenericJavaDemo4.java
@@ -1,8 +1,8 @@
 package genericjavademo4;
 
-interface A{}
-class B implements A{}
-class C implements A{}
+class A{}
+class B extends A{}
+class C extends B{}
 
 @SuppressWarnings("unchecked")
 class MyArray<T>
@@ -26,19 +26,49 @@ class MyArray<T>
 public class GenericJavaDemo4 {
     
     //GENERIC METHOD
-    static<E> void show(E[] list) //BOUND TYPES ALSO WORK: static<E extends Number> void show(E...list)
+    /*static<E> void show(E[] list) //BOUND TYPES ALSO WORK: static<E extends Number> void show(E...list)
     {
         for(E x:list)
         {
             System.out.println(x);
         }
+    }*/
+    
+    /*static void fun(MyArray obj)
+    {
+        obj.display();
+    }*/
+    
+    //UNBOUNDED WILDCARD
+    static void fun(MyArray<? super B> obj)
+    {
+        obj.display();
     }
 
     public static void main(String[] args) {
         // TODO code application logic here
         
-        show(new String[]{"Hii","Go","Bye"});
-        show(new Integer[]{10,20,30,40});
+        //1-GENERIC METHOD
+        //show(new String[]{"Hii","Go","Bye"});
+        //show(new Integer[]{10,20,30,40});
+        
+        //2-WILDCARD
+        /*MyArray<String> ma1=new MyArray<>();
+        ma1.append("Hi");
+        ma1.append("Bye");
+        
+        MyArray<Integer> ma2=new MyArray<>();
+        ma2.append(10);
+        ma2.append(20);
+        
+        fun(ma1);
+        fun(ma2);*/
+        
+        //3-WILDCARD
+        MyArray<B> ma1=new MyArray<>();
+        MyArray<C> ma2=new MyArray<>();
+        fun(ma1);
+        fun(ma2);
     }
     
 }
