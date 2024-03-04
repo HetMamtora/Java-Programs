baf4164cc8c75269a40896a5f601f528d37e1f06
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-04 16:57:21 +0530

    updated

---
+++ DateTime/src/datetime/DateTime.java
@@ -0,0 +1,31 @@
+package datetime;
+import java.util.*;
+
+public class DateTime {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //1-DATE REPRESENTATION
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.currentTimeMillis()/1000);
+        System.out.println(System.currentTimeMillis()/1000/60);
+        System.out.println(System.currentTimeMillis()/1000/60/60);
+        System.out.println(System.currentTimeMillis()/1000/60/60/24);
+        System.out.println(System.currentTimeMillis()/1000/60/60/24/365);
+        
+        System.out.println(Long.MAX_VALUE);
+        
+        //2-DATE CLASS
+        Date d=new Date(); //CONTAINS BOTH DATE & TIME
+        System.out.println(d);
+        System.out.println(d.getDate());
+        
+        Date d1=new Date("1/1/1980");
+        System.out.println(d1);
+        
+        //3-CALENDER CLASS
+        
+    }
+    
+}
