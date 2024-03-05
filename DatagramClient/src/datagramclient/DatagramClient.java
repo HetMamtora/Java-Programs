0c76f09bb809ec2b80659f1f1b8390d0ca2b1868
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-05 17:08:41 +0530

    updated

--- DatagramClient/src/datagramclient/DatagramClient.java
+++ DatagramClient/src/datagramclient/DatagramClient.java
@@ -24,5 +24,31 @@ public class DatagramClient {
         
         ds.close();
     }
+}
+
+//SERVER
+class Server 
+{  
+    public static void main(String[] args) throws Exception
+    {
+        DatagramSocket ds=new DatagramSocket(2000);
+        
+        byte b[]=new byte[1024];
+        DatagramPacket dp=new DatagramPacket(b,1024);
+        ds.receive(dp);
+        
+        String msg=new String(dp.getData()).trim();
+        System.out.println("From Client "+msg);
+        StringBuilder sb=new StringBuilder(msg);
+        sb.reverse();
+        msg=sb.toString();
+
+         dp=new DatagramPacket(msg.getBytes(),msg.length(),InetAddress.getByName("localhost"),2001);
+        
+        ds.send(dp);
+ 
+        ds.close();
+    
+    }
     
 }
