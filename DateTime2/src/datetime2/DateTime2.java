1c9e9a777ffb10b9cc9f9e55dc085500992d1380
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-05 15:18:43 +0530

    updated

---
+++ DateTime2/src/datetime2/DateTime2.java
@@ -0,0 +1,28 @@
+package datetime2;
+import java.util.*;
+import java.time.*;
+import java.time.temporal.*;
+
+public class DateTime2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ZonedDateTime zdt=ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println(zdt);
+        
+        OffsetDateTime odt=OffsetDateTime.now();
+        System.out.println(odt);
+        System.out.println(odt);
+        
+        MonthDay md=MonthDay.now();
+        System.out.println(md);
+        
+        Period p=Period.of(2, 2, 10);
+        System.out.println(p.addTo(LocalDate.now()));
+        
+        Instant i=Instant.now();
+        System.out.println(i);
+    }
+    
+}
