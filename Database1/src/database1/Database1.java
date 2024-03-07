4a43a6ca0c3b0db000544b6b2b8f92641100eb70
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-07 15:09:50 +0530

    Update Database1.java

--- Database1/src/database1/Database1.java
+++ Database1/src/database1/Database1.java
@@ -11,10 +11,26 @@ public class Database1 {
             Class.forName("org.sqlite.JDBC");
             Connection con = DriverManager.getConnection("jdbc:sqlite:D:/Projects/SQLite/univ.db");
 
-            Statement stm = con.createStatement();
+            //Statement stm = con.createStatement();
             
             //stm.executeUpdate("insert into dept values(60,'Chem')"); //DML QUERY 
-            //stm.executeUpdate("delete from dept where deptno>50"); //DML QUERY 
+            //stm.executeUpdate("delete from dept where deptno>50"); //DML QUERY
+            
+            //INSERT USING PREPARED STATEMENT
+            PreparedStatement stm = con.prepareStatement("insert into students values(?,?,?,?)");
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter student data: ");
+            int r=sc.nextInt();
+            String name=sc.next();
+            String city=sc.next();
+            int dno=sc.nextInt();
+            
+            stm.setInt(1, r);
+            stm.setString(2, name);
+            stm.setString(3, city);
+            stm.setInt(4, dno);
+            
+            stm.executeUpdate();
             
             stm.close();
             con.close();
