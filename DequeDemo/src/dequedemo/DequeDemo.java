6f38160e07e551429025cd93b90dd4d39fa6ef83
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-07 17:24:03 +0530

    updated

---
+++ DequeDemo/src/dequedemo/DequeDemo.java
@@ -0,0 +1,25 @@
+package dequedemo;
+import java.util.*;
+public class DequeDemo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ArrayDeque<Integer> dq=new ArrayDeque<>();
+         
+         dq.offerLast(10);
+         dq.offerLast(20);
+         dq.offerLast(30);
+         dq.offerLast(40);
+         
+         dq.pollFirst();
+         
+         dq.offerLast(1);
+         dq.offerLast(2);
+         dq.offerLast(3);
+         dq.offerLast(4);
+         
+         dq.forEach((x)->System.out.println(x));
+    }
+    
+}
