02a10ccd3028b6c94b2ae5645589152aab1b48f0
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-08 16:57:14 +0530

    Update HashSetDemo.java

--- HashSetDemo/src/hashsetdemo/HashSetDemo.java
+++ HashSetDemo/src/hashsetdemo/HashSetDemo.java
@@ -13,6 +13,17 @@ public class HashSetDemo {
         hs.add(10);
         
         System.out.println(hs);
+        
+        //TREE SET DEMO
+        TreeSet<Integer> ts=new TreeSet<>(List.of(10,30,50,70,10,40));
+        //Collecion<Integer> ts=new TreeSet<>(List.of(10,30,50,70,10,40));  ONLY THOSE METHODS THAT ARE IN COLLECTION
+        //Sorted<Integer> ts=new TreeSet<>(List.of(10,30,50,70,10,40));     ONLY THOSE METHODS THAT ARE IN SORTED
+        
+        ts.add(25);
+        
+        //ts.ceiling(55);
+        
+        System.out.println(ts);
     }
     
 }
