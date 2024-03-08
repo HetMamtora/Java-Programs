e1d3ce5f7758813c918218594b05412f20f30993
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-08 19:32:03 +0530

    updated

---
+++ BitSetDemo/src/bitsetdemo/BitSetDemo.java
@@ -0,0 +1,35 @@
+package bitsetdemo;
+import java.util.*;
+
+public class BitSetDemo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        BitSet bs1=new BitSet();
+        
+        bs1.set(0);
+        bs1.set(2);
+        bs1.set(4);
+        bs1.set(6);
+        bs1.set(8);
+           
+        BitSet bs2=new BitSet();
+       // bs2.set(0);
+        bs2.set(1);
+       // bs2.set(2);
+        bs2.set(3);
+       // bs2.set(4);
+        bs2.set(5);
+       // bs2.set(6);
+        bs2.set(7);
+       // bs2.set(8);
+        
+        bs1.and(bs2);
+        bs1.or(bs2);
+        
+        bs1.flip(0,bs1.length());
+        System.out.println(bs1);
+    }
+    
+}
