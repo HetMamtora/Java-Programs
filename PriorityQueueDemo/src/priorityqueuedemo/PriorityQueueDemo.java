a580f30d7bbb399b282fd15c889f6ef5fcaac231
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-08 16:32:28 +0530

    updated

---
+++ PriorityQueueDemo/src/priorityqueuedemo/PriorityQueueDemo.java
@@ -0,0 +1,38 @@
+package priorityqueuedemo;
+import java.util.*;
+
+class MyCom implements Comparator<Integer>
+{
+    public int compare(Integer o1,Integer o2)
+    {
+        if(o1<o2)return 1;
+        if(o1>o2) return -1;
+        return 0;
+    }
+}
+
+public class PriorityQueueDemo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        PriorityQueue<Integer> p=new PriorityQueue<>(new MyCom());
+        
+        p.add(20);
+        p.add(10);
+        p.add(30);
+        p.add(5);
+        p.add(15);
+        p.add(3);
+        
+        System.out.println(p.peek());
+        
+        p.forEach((x)->System.out.println(x));
+        
+        p.poll();
+        
+        System.out.println("After Deletion");
+        p.forEach((x)->System.out.println(x));
+    }
+    
+}
